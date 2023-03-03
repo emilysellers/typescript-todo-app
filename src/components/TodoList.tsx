310e5a9c88c3e48ffeaf7a78ec716d20d29f2445
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-02 16:25:30 -0800

    render containers for active and completed tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,16 +10,40 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos completed">
+        <span className="todos__heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
+    // <div className="todos">
+    //   {todos.map((todo) => (
+    //     <SingleTodo
+    //       todo={todo}
+    //       key={todo.id}
+    //       todos={todos}
+    //       setTodos={setTodos}
+    //     />
+    //   ))}
+    // </div>
   );
 };
 
